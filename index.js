94509f6468b796ee6dd7c5870e20783004caa164
EliasPeeters <Elias.peeters@me.com>
2020-01-06 23:53:12 +0100

    Fixed wrong strings

--- index.js
+++ index.js
@@ -597,20 +597,26 @@ app.post('/newmessage', urlencodedparser, async function (req, res) {
 	if (!logined(req, res)) {
 		return
 	}
+	console.log(req.body.message);
 	const user = await connection.asyncquery('SELECT * FROM theSchool.user WHERE userID = ' + loggedInUsers[req.cookies.sessionToken]);
 
 	const userNameTO = await connection.asyncquery('SELECT userID from user where userName = \''+req.body.username + '\'');
-	console.log(userNameTO);
 
 	if (userNameTO[0] === undefined) {
 		res.redirect('/newmessage?error=Den Benutzer gibt es nicht');
 		return
 	}
+
 	if (req.body.message === undefined || req.body.message === '') {
 		res.redirect('/newmessage?error=Die Nachricht hat keinen Inhalt');
 		return;
 	}
-	const query = await connection.asyncquery('INSERT INTO messages (messageContent, messageTime, messageFromUserID, messageToUserID) VALUES (\'' + req.body.message + '\', now(), ' + user[0].userID + ', ' + userNameTO[0].userID + ')')
+	try {
+		const query = await connection.asyncquery('INSERT INTO messages (messageContent, messageTime, messageFromUserID, messageToUserID) VALUES (\'' + req.body.message + '\', now(), ' + user[0].userID + ', ' + userNameTO[0].userID + ')')
+	} catch (e) {
+		res.redirect('/newmessage?error=Die Nachricht ist ungültig');
+	}
+
 	res.redirect('/message');
 });
 
